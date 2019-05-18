fc3d3a397ed82c20b6a2fdc7689ab4f7cbcd2490
Sathish <38915683+sathish-thangaraj@users.noreply.github.com>
2019-05-18 15:27:53 -0400

    Create UniqueChars.java

---
+++ leetcode/arrays/UniqueChars.java
@@ -0,0 +1,31 @@
+/*
+
+This class finds the unique chars in the string using boolean array
+
+String.charAt(position) method returns the character , When you assign the character to the integer value ,
+
+you will get the ASCII value of the character.
+ */
+
+public class UniqueChars {
+    public static void main(String[] args){
+        String s = "Aksith" ;
+        System.out.println("given string in " + isUniqueChars(s) );
+    }
+
+    public static boolean isUniqueChars(String input){
+        boolean[] char_set = new boolean[128] ;
+        for(int i=0 ; i<input.length() ; i++){
+            int val = input.charAt(i);
+            int val1 = '2' ;
+            System.out.println(val1);
+            if(char_set[val]){
+                return false;
+            }
+            else {
+                char_set[val] = true ;
+            }
+        }
+        return true ;
+    }
+}
